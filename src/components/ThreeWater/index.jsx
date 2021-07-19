664d83a83b5d1e18f7f6908a21dd2698bfab53c4
Dmarcotrigiano <daniel.marcotrigiano@gmail.com>
2021-07-19 19:59:46 -0400

    react-three/fiber fixes

--- src/components/ThreeWater/index.jsx
+++ src/components/ThreeWater/index.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { AmbientLight, PlaneGeometry } from "three";
-import { LiquidRefractionMaterial } from "./liquidMaterialShader";
+// import React from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { AmbientLight, PlaneGeometry } from "three";
+// import { LiquidRefractionMaterial } from "./liquidMaterialShader";
+
+// export const ThreeWater = ({}) => {
+//   return (
+//     <Canvas>
+//       <pointLight position={[]} />
+//       <mesh>
+//         <sphereBufferGeometry />
+//         <meshStandardMaterial color="hotpink" />
+//       </mesh>
+//     </Canvas>
+//     // <Canvas>
+//     //   <AmbientLight />
+//     //   <PlaneGeometry />
+//     //   <LiquidRefractionMaterial />
+//     // </Canvas>
+//   );
+// };
+// export default ThreeWater;
+import ReactDOM from "react-dom";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { InstancedMesh } from "three";
 
 export const ThreeWater = ({}) => {
   return (
     <Canvas>
-      <pointLight position={[]} />
       <mesh>
         <sphereBufferGeometry />
-        <meshStandardMaterial color="hotpink" />
+        <meshPhongMaterial color="blue" />
       </mesh>
+      <ambientLight />
     </Canvas>
-    // <Canvas>
-    //   <AmbientLight />
-    //   <PlaneGeometry />
-    //   <LiquidRefractionMaterial />
-    // </Canvas>
   );
 };
 export default ThreeWater;
